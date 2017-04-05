fe327265e6eaab71229d4a8d645ab76cb025f130
ibongiovanni <ignaciobongiovanni@gmail.com>
2017-04-05 14:24:22 -0300

    a

---
+++ P1/e5/e5.c
@@ -0,0 +1,6 @@
+int main(int argc, char const *argv[])
+{
+	write(1,"Hola \n",6);
+	write(2,"mundo \n",7);
+	return 0;
+}
---
+++ P1/e8/e8.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+l=0
+let t=0
+for i in $*; do
+	l=`wc -l $i | awk '{print $1}'`
+	echo $l
+	let t=$t+$l
+done
+
+echo "Total: $t, args:$#"
