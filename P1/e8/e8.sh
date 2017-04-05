537b11c772b5ce4019e27f7348219b650ffe8d79
ibongiovanni <ignaciobongiovanni@gmail.com>
2017-04-05 15:55:52 -0300

    a

--- P1/e8/e8.sh
+++ P1/e8/e8.sh
@@ -5,7 +5,7 @@ let t=0
 for i in $*; do
 	l=`wc -l $i | awk '{print $1}'`
 	echo $l
-	let t=$t+$l
+	let t=$t+$l #let es necesario para evaluar aritmeticamente
 done
 
 echo "Total: $t, args:$#"
--- P1/e9/system.c
+++ P1/e9/system.c
@@ -1,8 +1,25 @@
-int system (const char *cmd){
+#include <stdio.h>
+#include <unistd.h>
+
+int my_system (const char *cmd){
 	if( fork() == 0){
-		exec("/bin/bash", cmd);
+		//Child
+		printf("executing command: %s\n",cmd );
+		execl("/bin/bash","bash", "-c", cmd, (char *)0);
+		printf("Failed Execution\n" );
+		exit(-1);
 	}
 	else{
-		wait();
+		//father
+		int exit_status;
+		int child_pid;
+		child_pid=wait(&exit_status);
+		printf("\n child (%d) finishied with exit statatus: %d\n",child_pid,exit_status);
 	}
+}
+
+int main(int argc, char const *argv[])
+{
+	my_system(argv[1]);
+	return 0;
 }
