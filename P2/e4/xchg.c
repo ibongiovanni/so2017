b3bb52900b7fc57f1ab4a5b3aca44a526b1c34d7
ibongiovanni <ignaciobongiovanni@gmail.com>
2017-05-01 16:43:41 -0300

    a

--- P2/e4/xchg.c
+++ P2/e4/xchg.c
@@ -1,7 +1,7 @@
 static inline unsigned int xchg(volatile unsigned int * addr, unsigned int newval)
-{ unsigned int result;
- asm volatile ("xchg %0, %1":
-               "+m" (* addr),"=a" (result):
+{ unsigned int result;						//      (*addr)  (newval)
+ asm volatile ("xchg %0, %1":				//xchgl 8(%ebp), 12(%ebp) 
+               "+m" (* addr),"=a" (result): //mov eax -4(%ebp)
               "1" (newval) );
         return result ;
 }
